a0c6d0436e37b9a2c2cde66b7f9c623328995260
KapilThakkar <kapil@click2cloud.net>
2017-11-06 21:55:00 +0530

    codebase

--- PayloadData/azure-cosmos-db-graph-api/Web/GraphExplorer/App_Start/Startup.Auth.cs
+++ PayloadData/azure-cosmos-db-graph-api/Web/GraphExplorer/App_Start/Startup.Auth.cs
@@ -11,20 +11,19 @@ namespace GraphExplorer
 {
     public partial class Startup
     {
-        //// For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
+        // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
-				//Commented following lines of code to bypass authentication
-        //    app.UseWindowsAzureActiveDirectoryBearerAuthentication(
-        //        new WindowsAzureActiveDirectoryBearerAuthenticationOptions
-        //        {
-        //            Tenant = ConfigurationManager.AppSettings["ida:Tenant"],
-        //            TokenValidationParameters = new TokenValidationParameters
-        //            {
-        //                ValidAudience = ConfigurationManager.AppSettings["ida:Audience"]
-        //            },
-        //        }
-        //    );
+            // app.UseWindowsAzureActiveDirectoryBearerAuthentication(
+                // new WindowsAzureActiveDirectoryBearerAuthenticationOptions
+                // {
+                    // Tenant = ConfigurationManager.AppSettings["ida:Tenant"],
+                    // TokenValidationParameters = new TokenValidationParameters
+                    // {
+                        // ValidAudience = ConfigurationManager.AppSettings["ida:Audience"]
+                    // },
+                // }
+            // );
         }
     }
 }
